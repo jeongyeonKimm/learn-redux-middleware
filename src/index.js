25edcb9ef5e7839e5bfd535bc2555a136af0963a
kim_jeongyeon <wjddus9132@gmail.com>
2022-02-11 11:02:09 +0900

    Middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import rootReducer from "./modules";
+// import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
